cad27c56ef1b1599c646d5d87b4939551035e130
Raymond Feng <rfeng@us.ibm.com>
2016-03-15 16:49:45 -0700

    Remove regenerator from babel-runtime and bundle mssql

---
+++ remove-regenerator.js
@@ -0,0 +1,19 @@
+/**
+ * This script removes regenerator bundled with babel-runtime
+ */ 
+var fs = require('fs');
+try {
+  var index = require.resolve('babel-runtime/regenerator/index.js');
+  var runtime = require.resolve(
+    'babel-runtime/regenerator/runtime.js');
+  if (index) {
+    fs.unlink(index, function(err) {
+      if (err) console.error(err);
+      if (runtime) fs.unlink(runtime, function(err) {
+        if (err) console.error(err);
+      });;
+    });
+  }
+} catch (err) {
+  // Ignore
+}
