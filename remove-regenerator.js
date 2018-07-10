072ad8546b2cdf06116824b71e60122b7398357f
virkt25 <taranveer@virk.cc>
2018-07-09 20:20:16 -0400

    chore: update node + dependencies

--- remove-regenerator.js
+++ remove-regenerator.js
@@ -11,7 +11,8 @@ var fs = require('fs');
 try {
   var index = require.resolve('babel-runtime/regenerator/index.js');
   var runtime = require.resolve(
-    'babel-runtime/regenerator/runtime.js');
+    'babel-runtime/regenerator/runtime.js'
+  );
   if (index) {
     fs.unlink(index, function(err) {
       if (err) console.error(err);
